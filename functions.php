ebf0a5b4d151a7582d7712dda0ba59c2029e3471
Ross Wintle <ross@oikos.org.uk>
2017-11-10 17:44:11 +0000

    Tidy up and link to the dist css and js files

--- functions.php
+++ functions.php
@@ -117,11 +117,9 @@ add_action( 'widgets_init', 'oiko_s_widgets_init' );
  * Enqueue scripts and styles.
  */
 function oiko_s_scripts() {
-	wp_enqueue_style( 'oiko_s-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'oiko_s-style', get_stylesheet_directory_uri() . '/dist/style.css' );
 
-	wp_enqueue_script( 'oiko_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'oiko_s-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'we-are-tearfund-scripts', get_template_directory_uri() . '/dist/bundle.js', [ 'jquery' ], NULL, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
